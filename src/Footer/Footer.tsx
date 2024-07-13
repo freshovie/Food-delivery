fc7c5792a063c39dcb5ef5f99c8e71b596a28dd6
Egorke Ovie <egorkeovie@gmail.com>
2024-07-13 23:15:02 +0100

    navbar & footer animations

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -3,48 +3,86 @@ import "../Styles/footer.scss";
 import { Left } from "../assets";
 import { FaTwitter, FaFacebookSquare } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
+import { motion } from "framer-motion";
 
 function Footer() {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
   return (
-    <footer className="footer bg-body-tertiary text-center">
+    <motion.footer
+      className="footer bg-body-tertiary text-center"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="container p-4">
         <div className="row align-items-center">
           <div className="col-lg-6 col-md-12 mb-4 mb-lg-0">
-            <img
+            <motion.img
               src={Left}
               alt="Logo"
               onClick={scrollToTop}
               className="footer-logo"
+              initial={{ scale: 0.5 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.3 }}
             />
           </div>
           <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
-            <ul className="goIcons list-unstyled d-flex justify-content-center mb-0">
+            <motion.ul
+              className="goIcons list-unstyled d-flex justify-content-center mb-0"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+            >
               <li>
-                <FaTwitter className="icons" />
+                <motion.span
+                  initial={{ scale: 0.5 }}
+                  animate={{ scale: 1 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <FaTwitter className="icons" />
+                </motion.span>
               </li>
               <li>
-                <FaFacebookSquare className="icons" />
+                <motion.span
+                  initial={{ scale: 0.5 }}
+                  animate={{ scale: 1 }}
+                  transition={{ duration: 0.3, delay: 0.1 }}
+                >
+                  <FaFacebookSquare className="icons" />
+                </motion.span>
               </li>
               <li>
-                <RiInstagramFill className="icons" />
+                <motion.span
+                  initial={{ scale: 0.5 }}
+                  animate={{ scale: 1 }}
+                  transition={{ duration: 0.3, delay: 0.2 }}
+                >
+                  <RiInstagramFill className="icons" />
+                </motion.span>
               </li>
-            </ul>
+            </motion.ul>
           </div>
           <div className="col-lg-3 col-md-6">
-            <ul className="list-unstyled copy-write mb-0">
+            <motion.ul
+              className="list-unstyled copy-write mb-0"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.3 }}
+            >
               <li>
                 <a href="#!" className="text-body">
                   Copywright 2020 Bella Onojie.com
                 </a>
               </li>
-            </ul>
+            </motion.ul>
           </div>
         </div>
       </div>
-    </footer>
+    </motion.footer>
   );
 }
 
--- src/Navigation/Navbar.tsx
+++ src/Navigation/Navbar.tsx
@@ -2,15 +2,29 @@ import React from "react";
 import "../Styles/navbar.scss";
 import { Left } from "../assets";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
+import { motion } from "framer-motion";
 
-function Navbar() {
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+    <motion.nav
+      className="navbar navbar-expand-lg navbar-light bg-body-tertiary"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <motion.a
+          className="navbar-brand"
+          href="/"
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+        >
           <img src={Left} alt="Logo" />
-        </a>
-        <button
+        </motion.a>
+        <motion.button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
@@ -18,11 +32,25 @@ function Navbar() {
           aria-controls="navbarNavAltMarkup"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.3, delay: 0.5 }}
         >
           <HiOutlineMenuAlt1 />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex justify-content-center align-items-center">
+        </motion.button>
+        <motion.div
+          className="collapse navbar-collapse"
+          id="navbarNavAltMarkup"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.8 }}
+        >
+          <motion.div
+            className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex justify-content-center align-items-center"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+          >
             <a className="nav-link active" aria-current="page" href="/">
               Home
             </a>
@@ -35,11 +63,11 @@ function Navbar() {
             <a className="nav-link" href="/contact">
               Contact
             </a>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
-    </nav>
+    </motion.nav>
   );
-}
+};
 
 export default Navbar;
