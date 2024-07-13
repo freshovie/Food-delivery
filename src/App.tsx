9c67dd1b3163d1efd8534f982cf2c0aabc793fcd
Egorke Ovie <egorkeovie@gmail.com>
2024-07-13 13:06:49 +0100

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from "react";
-import HeroList from "./Components/HeroList";
-import Hero  from "./Models/Hero";
+import Display from "./Display";
 
-const heros: Hero[] = [{ id: 1, title: "Hero 1" }, { id: 2, title: "Hero 2" }];
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hero List</h1>
-
-      <header className="App-header">
-       Food Delivery 
-      </header>
-      <HeroList items={heros} />
+      <Display />
     </div>
   );
 };
---
+++ src/Display.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { HeroList } from "./Components";
+import Hero from "./Models/Hero";
+
+const heros: Hero[] = [
+  { id: 1, title: "Hero 1" },
+  { id: 2, title: "Hero 2" },
+];
+
+const Display: React.FC = () => {
+  return (
+    <div>
+      <h1>Hero List</h1>
+      <header className="App-header">Food Delivery</header>
+      <HeroList items={heros} />
+    </div>
+  );
+}
+
+export default Display;
